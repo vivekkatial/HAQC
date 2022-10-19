8a143b74c4808c5078ff322de6222b38250643f0
Vivek Katial <vivekkatial@gmail.com>
2022-10-20 01:25:42 +1100

    fet: nohup bash

--- bin/maxcut/run_maxcut_vm.sh
+++ bin/maxcut/run_maxcut_vm.sh
@@ -2,5 +2,5 @@
 for i in {1..150}
 do
    echo "Welcome this run:$i for MAXCUT VQE"
-   singularity run --app run_vqe_maxcut vqe_maxcut.img &
+   singularity run --app run_vqe_maxcut vqe_maxcut.img
 done
