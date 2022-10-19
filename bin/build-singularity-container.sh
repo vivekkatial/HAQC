12de93935f308ec4ae83affb40467acc6c6b2be5
Vivek Katial <vivekkatial@gmail.com>
2022-10-19 23:01:57 +1100

    feat: ADding build script for conainer

---
+++ bin/build-singularity-container.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+printf "\n       \\               ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ \n        \ji            ♥ Build Container ♥\n        /.(((          ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ \n       (,/\"(((__,--. \n           \  ) _( /{  \n           !|| \" :||    \n           !||   :||  \n           '''   '''  \n"
+
+singularity build SingularityFile.def vqe_maxcut.img
