6ce31c2e74b1ce5348e9e52a815861634e1cc650
Vivek Katial <vivekkatial@gmail.com>
2023-02-27 15:12:51 +1100

    feat: Adding Vanilla instance

--- bin/maxcut/qaoa_layers_vanilla/qaoa_vanilla_layers/run_maxcut_instances_vm.sh
+++ bin/maxcut/qaoa_layers_vanilla/qaoa_vanilla_layers/run_maxcut_instances_vm.sh
@@ -2,5 +2,5 @@
 for i in {1..150}
 do
    echo "Welcome this run:$i for MAXCUT QAOA number of Layers"
-   singularity run --app run_qaoa_maxcut_n_layers vqe_maxcut.img
+   singularity run --app run_qaoa_maxcut_layers vqe_maxcut.img
 done
