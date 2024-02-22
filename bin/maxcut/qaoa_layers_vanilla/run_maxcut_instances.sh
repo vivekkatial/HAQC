945a587db0a357225d4b12ed7c66008cdf3e0861
Vivek Katial <vivekkatial@gmail.com>
2024-02-22 13:44:33 +1100

    fix: Making some changes to definition file?

--- bin/maxcut/qaoa_layers_vanilla/run_maxcut_instances.sh
+++ bin/maxcut/qaoa_layers_vanilla/run_maxcut_instances.sh
@@ -1,30 +1,44 @@
 #!/bin/bash
 
 set -eo pipefail
-printf "\n       \\               ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ \n        \ji            ♥ Running VQE MAXCUT Experiments ♥\n        /.(((          ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ \n       (,/\"(((__,--. \n           \  ) _( /{  \n           !|| \" :||    \n           !||   :||  \n           '''   '''  \n"
+printf "\n       \\               ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ \n        \ji            ♥ Running MAXCUT Experiments ♥\n        /.(((          ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ \n       (,/\"(((__,--. \n           \  ) _( /{  \n           !|| \" :||    \n           !||   :||  \n           '''   '''  \n"
 
+# Define the array of node sizes
+node_sizes=(12)
 
-# Set variables
-export NodeMemory=40GB
+# Define the array of graph types
+graph_types=("Nearly Complete BiPartite" "Uniform Random" "Power Law Tree" "Watts-Strogatz small world" "3-Regular Graph" "4-Regular Graph" "Geometric")
 
-# Build random instances
-# echo "♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥"
-# echo "♥ Building Random Instances ... ♥"
-# echo "♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥"
+# Define the array of n_layers
+n_layers=(3)
 
-# sbatch --output=$log_file bin/run-experiments.slurm $exp_run_params
+# Initialize counter
+total_jobs=0
 
+# Main loop (running 100 instances of each node size, graph type and layer)
+for i in {1..300}; do
+   for node_size in "${node_sizes[@]}"; do
+      for graph_type in "${graph_types[@]}"; do
+         for layer in "${n_layers[@]}"; do
+               # Set NodeMemory based on node_size and aad
+               if [ "$node_size" -lt 15 ]; then
+                  NodeMemory="16GB"
+               else
+                  NodeMemory="40GB"
+               fi
 
-# echo "♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥"
-# echo "♥ Run Each Random Instance  ... ♥"
-# echo "♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥"
+               echo "Allocating node $NodeMemory memory for run number: $i, Node Size: $node_size, Graph Type: $graph_type, Layer: $layer"
+               log_file="logs/qaoa_maxcut_node_${node_size}_graph_${graph_type}_layer_${layer}_run_$i.log"
+               echo "Results will be logged into $log_file"
+               sbatch --chdir=$(pwd) --mem $NodeMemory --output="$log_file" bin/maxcut/run_maxcut.slurm $node_size "$graph_type" $layer
 
-
-for i in {1..200}
-do
-   echo "Allocating node $NodeMemory memory for run number: $i"
-   log_file="logs/qaoa_vanilla_layers_maxcut_run_all_instance_$i.log"
-   echo "Results will be logged into $log_file"
-   sbatch --mem $NodeMemory --output=$log_file bin/maxcut/qaoa_layers_vanilla/run_maxcut.slurm
-   # singularity run --app run_vqe_maxcut vqe_maxcut.img
+               # Increment the counter
+               total_jobs=$((total_jobs+1))
+               echo "Job number $total_jobs submitted"
+         done
+      done
+   done
 done
+
+# Display the total number of jobs submitted
+echo "Total number of jobs submitted: $total_jobs"
--- bin/maxcut/qaoa_layers_vanilla/run_maxcut_instances_vm.sh
+++ bin/maxcut/qaoa_layers_vanilla/run_maxcut_instances_vm.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-for i in {1..150}
+for i in {1..5}
 do
    echo "Welcome this run:$i for MAXCUT QAOA number of Layers"
-   singularity run --app run_qaoa_maxcut_layers vqe_maxcut.img
+   apptainer run \
+    --env MLFLOW_TRACKING_USERNAME=$MLFLOW_TRACKING_USERNAME \
+    --env MLFLOW_TRACKING_PASSWORD=$MLFLOW_TRACKING_PASSWORD \
+    --env AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+    --env AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+    --pwd /mnt \
+    --bind $(pwd):/mnt \
+    --app run_qaoa_maxcut_n_layers haqc.sif 12 "Nearly Complete BiPartite" 3 1000
 done
